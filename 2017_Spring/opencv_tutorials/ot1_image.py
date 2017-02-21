332711654ac1b018a1c7da8506bc3463896a437d
Eunjung <epark002@citymail.cuny.edu>
2017-02-21 11:06:54 -0500

    edit files for opencv tutorials upto second one.

---
+++ 2017_Spring/opencv_tutorials/ot1_image.py
@@ -0,0 +1,30 @@
+# ot1_image.py - read, display, and save image 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+# load a hand image
+img = cv2.imread('hand.png', cv2.IMREAD_GRAYSCALE) # apply gray filter
+	# other flags for imread()
+	# 	IMREAD_COLOR		OR 1
+	# 	IMREAD_UNCHANGED	OR -1
+
+# # display image
+# cv2.imshow('image', img)
+# cv2.waitKey(0) 
+	# wait for any key to be pressed infinitely
+	# waitKey(25) will display a frame for 25 ms, after which display will be automatically closed.
+# cv2.destroyAllWindows()
+
+# display image using matplotlib.pyplot
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+	# watch out when using color :
+	# - opencv uses BGR while matplotlib uses RGB
+
+# draw a line (x_points, y_points, cyan_color)
+plt.plot([50, 100], [80, 100], 'c', linewidth=5)
+plt.show()
+
+# save the gray filtered image
+cv2.imwrite('grayhand.png', img)
---
+++ 2017_Spring/opencv_tutorials/ot2_video.py
@@ -0,0 +1,57 @@
+# ot2_video.py - read, display, and save video 
+
+import cv2
+import numpy as np
+
+# load(a handle) video
+cap = cv2.VideoCapture(0) # 0 is the first web cam 
+
+# # 1. open a video frame
+# while True:
+# 	ret, frame = cap.read() # ret is true or false
+# 	cv2.imshow('frame', frame)
+
+# 	# display the video frame-by-frame for 1 ms
+# 	# & close the frame when 'q' is entered
+# 	if cv2.waitKey(1) & 0xFF == ord('q'): 
+# 		break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+# # 2. filter video into gray color
+# while True:
+# 	ret, frame = cap.read() # ret is true or false
+# 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# 	cv2.imshow('frame', frame)
+# 	cv2.imshow('gray', gray)
+
+# 	# display the video frame-by-frame for 1 ms
+# 	# & close the frame when 'q' is entered
+# 	if cv2.waitKey(1) & 0xFF == ord('q'): 
+# 		break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+# 3. save the video
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 2.0, (640, 480))
+
+while True:
+	ret, frame = cap.read() # ret is true or false
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	out.write(frame)
+
+	cv2.imshow('frame', frame)
+	cv2.imshow('gray', gray)
+
+	# display the video frame-by-frame for 1 ms
+	# & close the frame when 'q' is entered
+	if cv2.waitKey(1) & 0xFF == ord('q'): 
+		break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
