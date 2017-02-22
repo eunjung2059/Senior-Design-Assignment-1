d3ada77138826e2fe0ec76268607bb9f72f4eeb5
Eunjung <epark002@citymail.cuny.edu>
2017-02-21 20:46:34 -0500

    extract fourground manually

---
+++ 2017_Spring/opencv_tutorials/ot12_foreground.py
@@ -0,0 +1,24 @@
+# ot12_foreground.py -  find foreground(remove background) manually
+# ref 
+# - https://pythonprogramming.net/grabcut-foreground-extraction-python-opencv-tutorial/
+
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt
+
+img = cv2.imread('opencv-python-foreground-extraction-tutorial.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+
+# rect = (161, 79, 150, 150) # rectangle around head
+rect = (50, 50, 300, 500) # rectangle around body
+
+# remove background around rect
+cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
+img = img*mask2[:, :, np.newaxis]
+plt.imshow(img)
+plt.colorbar()
+plt.show()
