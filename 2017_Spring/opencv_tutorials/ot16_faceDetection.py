817c41e74803c7366b14d86b7a5d30c49956ae2b
Eunjung <epark002@citymail.cuny.edu>
2017-02-22 01:36:20 -0500

    notes on creating Haar Cascade

--- 2017_Spring/opencv_tutorials/ot16_faceDetection.py
+++ 2017_Spring/opencv_tutorials/ot16_faceDetection.py
@@ -19,18 +19,25 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # detect face on webcam input
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+
+        # in face
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
+        # detect eyes
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
 
     cv2.imshow('img',img)
+
     k = cv2.waitKey(30) & 0xff
     if k == 27: # ESC key
         break
---
+++ 2017_Spring/opencv_tutorials/ot17to20_HaarCascade.py
@@ -0,0 +1,89 @@
+# ot17to20_HaarCascade.py - create Haar Cascades for hand detection
+# ref - https://pythonprogramming.net/haar-cascade-object-detection-python-opencv-tutorial/
+
+''' 
+ Steps to make our own Haar Cascades
+1. Collect thousands of "negative" or "background" images - target object is not included
+		bg.txt file contains path to background images
+			neg/1.jpg
+			neg/2.jpg
+			...
+2. Collect or create "positive" images - target object
+		pos.txt(info.txt) file contains path to negative images, num objects, start pt, rectangle coordinates
+			pos/1.jpg 1 0 0 50 50  
+			pos/2.jpg
+			...
+3. create a positive vector file by stitching together all positives
+4. train cascade - with num_pos(50x50) = 2 * num_neg(100x100)
+'''
+### download-image-by-link.py ###
+import urllib.request
+import cv2
+import numpy as np 
+import os
+
+def store_raw_images():
+    neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'   
+    neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
+    pic_num = 1
+    
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+        
+    for i in neg_image_urls.split('\n'):
+        try:
+            print(i)
+            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
+
+            img = cv2.imread("neg/"+str(pic_num)+".jpg", cv2.IMREAD_GRAYSCALE)
+
+            # should be larger than samples / pos pic (so we can place our image on it)
+            resized_image = cv2.resize(img, (100, 100))
+
+            cv2.imwrite("neg/"+str(pic_num)+".jpg", resized_image)
+            pic_num += 1
+            
+        except Exception as e:
+            print(str(e)) 
+
+store_raw_images()
+
+# delete ugly files
+def find_uglies():
+    match = False
+    for file_type in ['neg']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir('uglies'):
+                try:
+                    current_image_path = str(file_type)+'/'+str(img)
+                    ugly = cv2.imread('uglies/'+str(ugly))
+                    question = cv2.imread(current_image_path)
+
+                    # are they same images?
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+
+                except Exception as e:
+                    print(str(e))
+find_uglies()
+
+def create_pos_n_neg():
+    for file_type in ['neg']: # can add 'pos' later
+        
+        for img in os.listdir(file_type):
+
+            if file_type == 'neg':
+                line = file_type + '/' + img + '\n'
+                with open('bg.txt','a') as f:
+                    f.write(line) 
+
+            # elif file_type == 'pos':
+            #     line = file_type + '/' + img + ' 1 0 0 50 50\n'
+            #     with open('info.dat','a') as f:
+            #         f.write(line)
+
+create_pos_n_neg()
+
+### opencv tutorial 20 is about creanting samples using opencv_createsamples
