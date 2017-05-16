afd46642c3fa1a6e78286b8a84057f9f93f2db68
Eunjung <epark002@citymail.cuny.edu>
2017-05-16 19:38:57 -0400

    upgrade getdata.py

--- 2017_Spring/progress/getdata.py
+++ 2017_Spring/progress/getdata.py
@@ -3,29 +3,47 @@ import cv2
 import numpy as np
 import io
 
+############################ filename ######################################
+tmpl = cv2.imread('template2/one.png',0) # 0 to Loads image in grayscale mode
+
+
 # load(a handle) video
 cap = cv2.VideoCapture(0) # 0 is the first web cam 
 
+#set the width and height of the capture
+#cap.set(3,200) # CV_PROP_FRAME_WIDTH
+#cap.set(4,250) # CV_PROP_FRAME_HEIGHT
+
 i = 1
-while (True or (i >= 1000)):
+while (True and (i <= 1000)):
 	ret, frame = cap.read() # ret is true or false
-	
-	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# 1. draw a rectangle ROI - Region Of Interest is (50, 200), (250, 450)
 	cv2.rectangle(frame, (50, 200), (250, 450), (0, 255, 0), 3)
 	roi = frame[200:450, 50:250] # [y points, x points]
-	
-	#cv2.imwrite('roi.png', roi)
-	#------------------------------------------------------------------------
-	cv2.imwrite("data/one_" + str(i) + ".png", roi)
-	i = i + 1
-	#------------------------------------------------------------------------
 
+	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+	#cv2.imwrite('roi.png', roi)
+	
 	# 2. write some text
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	# putText(where, what, loc, style, size, BGR, thickness, lineType)
 	cv2.putText(frame, '"Enter q to quit."', (10, 50), font, 1, (0, 255, 0), 1, cv2.LINE_AA) 
+	
+	### 3. template match ###
+	res1 = cv2.matchTemplate(gray, tmpl, cv2.TM_CCOEFF_NORMED)
+	
+	# find by adjusting the threshold value
+	threshold = 0.66
+	loc1 = np.where(res1 >= threshold)
+	print(loc1)
+	for pt in zip(*loc1[::-1]):	#------------------------------------------------------------------------
+##################################### filename_ #########################
+		cv2.imwrite("data/" + "one_" + str(i) + ".png", roi)
+		i = i + 1	#------------------------------------------------------------------------
+
+	#out.write(frame)
 
 	cv2.imshow('frame', frame)
 
