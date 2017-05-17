1dd62b8ef8566494f8f32e1496e2c8b2cd3303c6
Eunjung <epark002@citymail.cuny.edu>
2017-05-17 02:09:30 -0400

    modify load_data2.py to have y label on top of dataset(images)

--- 2017_Spring/progress/load_data2.py
+++ 2017_Spring/progress/load_data2.py
@@ -1,4 +1,4 @@
-# load_data.py - load 3000 dataset and use them to train and test
+# load_data2.py - load 3000 dataset and use them to train and test
 import numpy as np
 #import matplotlib.pyplot as plt
 import cv2
@@ -13,4 +13,12 @@ print(file_names[0:10]) # confirm first 11 file names
 
 
 images = io.imread_collection(file_names)
-print(images[0])
+print(images.files[0]) # should be 'data/one_1.png'
+print(images.files[1000]) # should be 'data/three_1.png'
+print(images.files[2000]) # should be 'data/two_1.png'
+
+arr_1s = np.ones((1000,), dtype=np.int)
+arr_2s = arr_1s + arr_1s
+arr_3s = arr_2s + arr_1s
+
+y = np.append(arr_1s, [arr_2s, arr_3s]) # y contains 1000 1s, 1000 2s, 1000 3s in this order  
