86afa85a29bf3373ab69dc4896b7cac689fae88c
Eunjung <epark002@citymail.cuny.edu>
2017-05-17 05:40:29 -0400

    modify load_data.py to do KNN

--- 2017_Spring/progress/load_data.py
+++ 2017_Spring/progress/load_data.py
@@ -5,17 +5,19 @@ import cv2
 import glob
 
 file_names = glob.glob("data/*.png")
-print(len(file_names)) 	# confirm the total number of file paths
+print("Loading " + str(len(file_names)) + " files.") 	# confirm the total number of file paths
 file_names = np.sort(file_names)
-print(file_names[0:10]) # confirm first 11 file names
+#print(file_names[0:10]) # confirm first 11 file names
 
 # read the first image
 f = file_names[0]	
 #print(f)
 img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
-img_list = np.array([img]) # image is 250*200
-#print(len(img_list))
-#print(img_list)
+images = np.array([img.flatten()]) # image is 250*200
+#print(len(images))
+#print(images[0].shape)
+#print(images[0])
+
 file_names = file_names[1:]
 #print(len(file_names))
 
@@ -23,6 +25,47 @@ file_names = file_names[1:]
 for f in file_names:
 	img = cv2.imread(f, cv2.IMREAD_GRAYSCALE) # apply gray filter
 	#print(img)
-	img_list = np.append(img_list, [img], axis=0)
+	images = np.append(images, [img.flatten()], axis=0)
 	
-print(len(img_list)) # on successful load, total should be 3000
+#print(len(images)) 
+print("total images: %d"%(len(images))) # should be 3000
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# make an array of labels
+arr_1s = np.ones((1000,), dtype=np.int)
+arr_2s = arr_1s + arr_1s
+arr_3s = arr_2s + arr_1s
+
+y = np.append(arr_1s, [arr_3s, arr_2s]) # y contains 1000 1s, 1000 3s, 1000 2s in this order
+
+# split the dataset "images" and label "y"
+x_train, x_test, y_train, y_test = train_test_split(images, y, test_size=0.2, random_state=42)
+print("split training images: %d"%(len(x_train))) # should be 2400
+
+
+num_neigh = [1, 3, 5, 7, 9]
+
+for num in num_neigh:
+	# Train
+	print("Training using KNN with %d neighbors."%(num))
+	neigh = KNeighborsClassifier(n_neighbors=num)
+	neigh.fit(x_train, y_train) 
+
+	# Test
+	print("Testing.")
+	guesses = neigh.predict(x_test)
+	#pred_prob = neigh.predict_proba(x_test)
+	#print(guesses)
+	#print(y_test)
+	#print(pred_prob)
+
+	# calculate correctness in percetage
+	correct = np.sum(np.equal(y_test, guesses))
+	print("Among %d testing %d were correct."%(len(y_test), correct))
+
+	correct_percent = correct/(len(y_test))*100.0
+	print(correct_percent)
+	print("Correct %7.2f on test set."%(correct_percent))
+
--- 2017_Spring/progress/load_data2.py
+++ 2017_Spring/progress/load_data2.py
@@ -4,24 +4,42 @@ import numpy as np
 import cv2
 import glob
 from skimage import io
-
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 file_names = glob.glob("data/*.png")
-print(len(file_names)) 	# confirm the total number of file paths
+# print(len(file_names)) 	# confirm the total number of file paths
 file_names = np.sort(file_names)
-print(file_names[0:10]) # confirm first 11 file names
+# print(file_names[0:10]) # confirm first 11 file names
 
 
 images = io.imread_collection(file_names)
-print(images.files[0]) # should be 'data/one_1.png'
-print(images.files[1000]) # should be 'data/three_1.png'
-print(images.files[2000]) # should be 'data/two_1.png'
+#print(images.files[0]) # should be 'data/one_1.png'
+#print(images.files[1000]) # should be 'data/three_1.png'
+#print(images.files[2000]) # should be 'data/two_1.png'
+#print(images[0].shape)
 
 arr_1s = np.ones((1000,), dtype=np.int)
 arr_2s = arr_1s + arr_1s
 arr_3s = arr_2s + arr_1s
 
 y = np.append(arr_1s, [arr_3s, arr_2s]) # y contains 1000 1s, 1000 3s, 1000 2s in this order
-print(y[:10])
-print(y[999:1010])
-print(y[1998:2010])
+
+# split the dataset "images" and label "y"
+x_train, x_test, y_train, y_test = train_test_split(images, y, test_size=0.2, random_state=42)
+
+
+#neigh = KNeighborsClassifier(n_neighbors=3)
+#neigh.fit(x_train, y_train) 
+#guesses = neigh.predict(x_test)
+#pred_prob = neigh.predict_proba(x_test)
+#print(guesses)
+#print(pred_prob)
+knn = cv2.ml.KNearest_create()
+#knn.train(x_train,y_train)
+knn.train(x_train,cv2.ml.ROW_SAMPLE,y_train)
+ret, results, neighbours ,dist = knn.find_nearest(y_test, 3)
+
+print "result: ", results,"\n"
+print "neighbours: ", neighbours,"\n"
+print "distance: ", dist
